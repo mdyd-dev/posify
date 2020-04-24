48b1f3b6b7976a397b3af6e4519212afc82141cf
Paweł Kowalski <pavelloz@gmail.com>
2020-04-25 01:23:53 +0200

    Cleanup scraper plugins

--- src/commands/download.js
+++ src/commands/download.js
@@ -4,7 +4,6 @@ const URL = require("url");
 const root = require("root-domain");
 const scrape = require("website-scraper");
 const SaveToExistingDirectoryPlugin = require("website-scraper-existing-directory");
-// const HtmToHtml = require("../lib/scraper-plugins/htm-to-html");
 const GenerateFilename = require("../lib/scraper-plugins/generate-filename");
 
 const ora = require("ora");
@@ -58,7 +57,6 @@ const download = (url, { concurrency }) => {
     directory: domain,
     plugins: [
       new SaveToExistingDirectoryPlugin(),
-      // new HtmToHtml(),
       new GenerateFilename(),
     ],
   });
